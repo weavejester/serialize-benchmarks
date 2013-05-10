927f134087125e50ac59f61b010123d31ae8717b
James Reeves <jreeves@weavejester.com>
2013-05-10 16:47:57 +0100

    Added reader tests

--- src/test_serialize/core.clj
+++ src/test_serialize/core.clj
@@ -3,7 +3,8 @@
             [carbonite.api :as carbonite]
             [carbonite.buffer]
             [deep-freeze.core :as deep-freeze]
-            [taoensso.nippy :as nippy]))
+            [taoensso.nippy :as nippy]
+            [clojure.edn :as edn]))
 
 (def test-data
   {:nil          nil
@@ -34,17 +35,50 @@
 (def carbonite-registry
   (carbonite/default-registry))
 
-(defn carbonite-write []
-  (carbonite.buffer/write-bytes carbonite-registry test-data))
+(defn carbonite-write [data]
+  (carbonite.buffer/write-bytes carbonite-registry data))
 
-(defn deep-freeze-write []
-  (deep-freeze/freeze-to-array test-data))
+(defn deep-freeze-write [data]
+  (deep-freeze/freeze-to-array data))
 
-(defn nippy-write []
-  (nippy/freeze-to-bytes test-data))
+(defn nippy-write [data]
+  (nippy/freeze-to-bytes data))
 
-(defn pr-byte-array-write []
+(defn pr-byte-array-write [data]
   (let [stream (java.io.ByteArrayOutputStream.)
         writer (java.io.BufferedWriter. (java.io.OutputStreamWriter. stream))]
-    (binding [*out* writer] (pr test-data))
+    (binding [*out* writer] (pr data))
+    (.flush writer)
     (.toByteArray stream)))
+
+(def carbonite-read-data
+  (carbonite-write test-data))
+
+(def deep-freeze-read-data
+  (deep-freeze-write test-data))
+
+(def nippy-read-data
+  (nippy-write test-data))
+
+(def pr-read-data
+  (pr-byte-array-write test-data))
+
+(defn carbonite-read [data]
+  (carbonite.buffer/read-bytes carbonite-registry data))
+
+(defn deep-freeze-read [data]
+  (deep-freeze/thaw-from-array data))
+
+(defn nippy-read [data]
+  (nippy/thaw-from-bytes data))
+
+(defn read-bytes [read-func data]
+  (let [stream (java.io.ByteArrayInputStream. data)
+        reader (java.io.BufferedReader. (java.io.InputStreamReader. stream))]
+    (read-func (java.io.PushbackReader. reader))))
+
+(defn clojure-default-read [data]
+  (read-bytes read data))
+
+(defn clojure-edn-read [data]
+  (read-bytes edn/read data))
